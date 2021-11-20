a41ac7d53620c4db64fde702d2e9f82e343df5da
SectorArcher <sectorarcher@gmail.com>
2021-11-20 13:25:25 +0100

    add

--- Elso/venv/secret_logic.py
+++ Elso/venv/secret_logic.py
@@ -1,6 +1,3 @@
-
-
-
 def is_numeric(text):
     return text.isnumeric()
 
@@ -9,14 +6,16 @@ def is_supported_operator(text):
     return text in ['+', '-', '*', '/']
 
 
-def calculate(operand1, operator, operand2):
-    rw =0
-    if operator == '+':
-            rw = operand1 + operand2
-    elif operator == '-':
-            rw = operand1 - operand2
-    elif operator == '*':
-            rw = operand1 * operand2
-    elif operator == '/':
-            rw = operand1 / operand2
-    return rw
+def calculate(operand1, l_operator, operand2):
+    result = 0
+
+    if l_operator == '+':
+        result = operand1 + operand2
+    elif l_operator == '-':
+        result = operand1 - operand2
+    elif l_operator == '*':
+        result = operand1 * operand2
+    elif l_operator == '/':
+        result = operand1 / operand2
+
+    return result
